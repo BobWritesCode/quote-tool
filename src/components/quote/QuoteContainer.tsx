e60dde9bef9b7bd5bae878ea7c75c00ca64c824a
BobWritesCode <warwick_hart@hotmail.com>
2023-09-10 14:54:12 +0100

    feat: add start date input

--- src/components/quote/QuoteContainer.tsx
+++ src/components/quote/QuoteContainer.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import appStyles from '../../styles/App.module.css';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import productsData from '../../data/products.json';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 interface ProductData {
   [key: string]: any;
@@ -20,17 +21,28 @@ type Props = {
 
 const QuoteContainer = (props: Props) => {
   const { quoteData, onAddQuoteSlot, onRemoveQuoteSlot } = props;
+
+  const wasStartDateSelected = useRef(false);
+
   const data: ProductData = productsData;
   const [range, setRange] = useState('');
   const [product, setProduct] = useState('');
   const [showProductRangeSelection, SetShowProductRangeSelection] =
     useState(false);
+  const currentDate = new Date(); // Create a new Date object for the current date
+  const formattedCurrentDate = currentDate.toISOString().substr(0, 10); // Format it as "YYYY-MM-DD"
+  const [startDate, setStartDate] = useState('');
 
   const handleRangeChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setRange(e.target.value);
+    wasStartDateSelected.current = true;
   };
   const handleProductChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     setProduct(e.target.value);
+  };
+
+  const handleStartDateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setStartDate(e.target.value);
   };
 
   const handleAddQuote = () => {
@@ -43,6 +55,10 @@ const QuoteContainer = (props: Props) => {
     onRemoveQuoteSlot(quoteData.temp_quote_id);
   };
 
+  const handleSetDateToToday = () => {
+    setStartDate(formattedCurrentDate);
+    wasStartDateSelected.current = true;
+  };
   return (
     <div className={`${appStyles.Box} mb-2 p-3`}>
       {!showProductRangeSelection && (
@@ -51,6 +67,29 @@ const QuoteContainer = (props: Props) => {
         </Button>
       )}
       {showProductRangeSelection && (
+        <div className="d-flex mb-2">
+          {/* Input for start date */}
+          <FloatingLabel
+            controlId="floatingInput"
+            label="Start Date"
+            className="me-2"
+          >
+            <Form.Control
+              type="date"
+              name="start_date"
+              placeholder="Start date"
+              value={startDate}
+              onChange={handleStartDateChange}
+            />
+          </FloatingLabel>
+
+          <Button variant="primary" onClick={handleSetDateToToday}>
+            Today
+          </Button>
+        </div>
+      )}
+
+      {wasStartDateSelected.current && (
         <Form.Select
           aria-label="Select product range"
           className="mb-2"
